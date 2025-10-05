739068133a2ff45cc84797954b5d584a78bf4f53
forest-eloghosa <foresteloghosa@gmail.com>
2025-10-05 12:41:05 +0100

    Add migrations for post excerpt and update files

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as index_views
+from blog import views as blog_views
 
 urlpatterns = [
-    path('', index_views.index, name='home'),
-    path('blog/', index_views.index, name='blog'),
+    path('', blog_views.my_blog, name='home'),
     path('admin/', admin.site.urls),
+    path('blog/', blog_views.my_blog, name='blog'),
 ]
