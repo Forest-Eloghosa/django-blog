cdaf0de118c329dd5a27e4138d5fa5ea87a3a04c
forest-eloghosa <foresteloghosa@gmail.com>
2025-10-06 19:34:14 +0100

    Fix 0005 dependency to use remaining 0004 filename

--- blog/migrations/0005_alter_comment_options.py
+++ blog/migrations/0005_alter_comment_options.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blog', '0004_alter_post_options_remove_comment_new_field'),
+        ('blog', '0004_alter_comment_options_alter_post_options_and_more'),
     ]
 
     operations = [
