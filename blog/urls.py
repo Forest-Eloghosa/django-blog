77cf43d5d567290ce9194d6b1144ea2c70502270
forest-eloghosa <foresteloghosa@gmail.com>
2025-10-19 16:44:53 +0100

    added comment CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name="home"),
-    path("posts/<slug:slug>/", views.post_detail, name="post_detail"),
-    path("events/<int:event_id>/", views.event_detail, name="event_detail"),
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('events/<int:event_id>/', views.event_detail, name="event_detail"),
+    path('posts/<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>/', views.comment_delete, name='comment_delete'),
 ]
